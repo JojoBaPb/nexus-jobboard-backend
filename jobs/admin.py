c8a462a07908cea1f08078dcd1c298770a7d183d
JojoBaPb <sipo.jmudenda@gmail.com>
2025-09-21 21:47:24 +0200

    job categories added

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Company, Job
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Job)
+
