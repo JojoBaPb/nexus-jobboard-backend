a04f680f75b6e2668654d46340f9d8415e43d331
JojoBaPb <sipo.jmudenda@gmail.com>
2025-09-24 12:30:16 +0200

    urls update

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,26 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, LogoutView, ChangePasswordView
+from .views import (
+    RegisterView,
+    ProfileView,
+    LogoutView,
+    ChangePasswordView,
+)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # User registration
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+
+    # JWT login & refresh
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # User profile (get/update)
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+    # Logout (blacklist refresh token)
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # Change password
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
